5f60a0923d9c0c92718bdf02a7ceb6035a7b9754
Ranganathan7 <51285736+Ranganathan7@users.noreply.github.com>
2023-04-28 14:46:44 +0530

    updated backend endpoint

--- src/components/apiCalls/ENDPOINT.ts
+++ src/components/apiCalls/ENDPOINT.ts
@@ -1,2 +1,2 @@
-export const endpoint = "https://chat-mate-backend.onrender.com"
+export const endpoint = "https://chat-mate-backend-xpq1.onrender.com"
 // export const endpoint = "http://localhost:5000"
